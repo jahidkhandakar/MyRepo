14b1db414609244a797a6485b2efa6020bbc757c
jahid <jahid50k@gmail.com>
2022-01-06 22:40:22 +0600

    first commit

---
+++ TestClass.java
@@ -0,0 +1,16 @@
+
+package javaapplication23;
+
+public class TestClass {
+    int id;
+    String name;
+    
+    
+    TestClass(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    
+    
+    
+}
---
+++ test.java
@@ -0,0 +1,12 @@
+
+package javaapplication23;
+
+public class test {
+    
+    public static void main(String[] args) {
+        TestClass tc = new TestClass(1,"jahid");
+        
+        System.out.println(tc.id+" "+tc.name);
+    }
+    
+}
